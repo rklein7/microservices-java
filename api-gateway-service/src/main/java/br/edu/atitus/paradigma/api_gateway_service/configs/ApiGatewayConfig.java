145d96c6a2dac6ce90e4d8f535655525afd81e20
Rafael Klein <rafael.augustoklein04@gmail.com>
2024-10-24 20:31:23 -0300

    Aula 24/10 intervalo

---
+++ api-gateway-service/src/main/java/br/edu/atitus/paradigma/api_gateway_service/configs/ApiGatewayConfig.java
@@ -0,0 +1,29 @@
+package br.edu.atitus.paradigma.api_gateway_service.configs;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiGatewayConfig {
+
+    @Bean
+    RouteLocator getRoutes(RouteLocatorBuilder builder){
+        return builder.routes()
+                .route(rota -> rota
+                        .path("/get")
+                        .filters(f -> f
+                                .addRequestHeader("Usuario", "Fulano")
+                                .addRequestParameter("Parametro", "ValorParametro")
+                                .addResponseHeader("Servidor", "meuServer"))
+                        .uri("http://httpbin.org"))
+                .route(rota -> rota
+                        .path("/cambio-service/**")
+                        .uri("lb://cambio-service"))
+                .route(rota -> rota
+                        .path("/produto-service/**")
+                        .uri("lb://produto-service"))
+                .build();
+    }
+}
